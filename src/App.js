c02f582107ef6ef4b997ca39d53efbb9a4887285
ok <kelvin147789@gmail.com>
2020-12-23 18:33:27 +0800

    copy navbar finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './index.css'
 
 import {ReactComponent as BellIcon} from './icons/bell.svg'
@@ -9,58 +9,157 @@ import {ReactComponent as CogIcon} from './icons/cog.svg'
 import {ReactComponent as ChevronIcon} from './icons/chevron.svg'
 import {ReactComponent as ArrowIcon} from './icons/arrow.svg'
 import {ReactComponent as BoltIcon} from './icons/bolt.svg'
+import { Icon } from '@material-ui/core';
+
+import {CSSTransition} from 'react-transition-group';
 
 
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <div className="title">DeFi Lab</div>
-    <Navbar>
-      <NavItem icon= {<PlusIcon/>}/>
-      <NavItem icon={<BellIcon/>}/>
-      <NavItem icon={<MessengerIcon/>}/>
-    </Navbar>
-
-    <Context></Context>
     
-    </div>
+    <div>
 
- 
+<div className="title">DeFi Lab</div>
+    <Navbar>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
+
+     
+    </Navbar>
+    <Context></Context>
+    </div>
+    
+
    
-  )
+  );
 }
 
-const Navbar= (props)=> {
-  return(
+function Navbar(props) {
+  return (
     <nav className="navbar">
-  
       <ul className="navbar-nav">{props.children}</ul>
     </nav>
-  
-
   );
 }
 
-const NavItem= (props)=> {
-  return(
+function NavItem(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
     <li className="nav-item">
-
-      
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
-        
       </a>
-    </li>
-  
 
+      {open && props.children}
+    </li>
   );
 }
 
-const Context = (props)=> {
+function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight)
+  }, [])
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+
+      <CSSTransition
+        in={activeMenu === 'main'}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings">
+            Settings
+          </DropdownItem>
+
+
+          <DropdownItem
+            leftIcon="🦧"
+            rightIcon={<ChevronIcon />}
+            goToMenu="animals">
+            Animals
+          </DropdownItem>
+
+
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'settings'}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+            <h2>My Tutorial</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>HTML</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>CSS</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>JavaScript</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Awesome!</DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === 'animals'}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}>
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+            <h2>Animals</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon="🦘">Kangaroo</DropdownItem>
+          <DropdownItem leftIcon="🐸">Frog</DropdownItem>
+          <DropdownItem leftIcon="🦋">Horse?</DropdownItem>
+          <DropdownItem leftIcon="🦔">Hedgehog</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  );
+}
+
+function Context(props) {
   return(
     <div className="context">
+      <div className="spacing-bottom">
       Welcome to DeFi Lab, enjoy experiment here.
+      </div>
 
      <div>
      <button class="cybr-btn">
@@ -74,4 +173,9 @@ const Context = (props)=> {
   )
 }
 
-export default App
+export default App;
+
+
+
+
+
